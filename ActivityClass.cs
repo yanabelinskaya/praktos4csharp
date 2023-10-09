62da03b6b9b9703d61e13f6f9eb6ac419dc774f4
yanabelinskaya <144311460+yanabelinskaya@users.noreply.github.com>
2023-10-09 13:44:59 +0300

    praktos4
    -

---
+++ ActivityClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Activity
+{
+    public string Titles { get; set; }
+    public string Descriptions { get; set; }
+    public string Locations { get; set; }
+
+    public Activity(string title, string description, string location)
+    {
+        Titles = title;
+        Descriptions = description;
+        Locations = location;
+    }
+}
+
